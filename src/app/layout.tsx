85c34888d5584cd25eb1883de6baf0ed1cc218d2
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-09-14 13:36:04 +0800

    feat: add alpino font and re generate yarn lock file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,23 @@
+import localFont from "next/font/local";
+
+// Font files can be colocated inside of `app`
+const alpino = localFont({
+  src: "../../public/fonts/Alpino-Variable.woff2",
+  display: "swap",
+  weight: "100 900",
+  variable: "--font-alpino",
+});
+
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
-import "./app.css"
+import "./app.css";
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${alpino.variable}`}>
       <body>{children}</body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
